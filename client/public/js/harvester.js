2e70e3fff0584279776d4b1ea4bc00e316802675
Duong Van Sang <sangdv2703@gmail.com>
2018-12-07 09:57:05 +0700

    Update UI

--- client/public/js/harvester.js
+++ client/public/js/harvester.js
@@ -5,5 +5,23 @@ $(document).ready(function(){
     // applySelectBoxType("productId");
     // applySelectBoxType("packageType");
 
-    
+    let scanner = new Instascan.Scanner({ 
+        video: document.getElementById('preview') 
+    });
+    scanner.addListener('scan', function (content) {
+        // query information from blockchain
+        $("#qrCodeId").val(content);
+        scanner.stop();
+    });
+    $("#scan").click(()=> {
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (e) {
+            console.error(e);
+        });
+    });
 });
